aa45b97f00e706a2c07dc628fd2d9307492a401a
Brennon Toomepuu <btoomepuu@gmail.com>
2022-06-13 14:36:06 +0000

    completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,15 @@
+/* eslint-disable prefer-const */
+let languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+
+let firstElement = languages[0];
+console.log(`the first element of the array is ${firstElement}`);
+
+let thirdElement = languages[2];
+console.log(`the first element of the array is ${thirdElement}`);
+
+let length = languages.length;
+console.log(`the length of the array is ${length}`);
+
+let lastElement = languages[languages.length - 1];
+console.log(`the last element of the array is ${lastElement}`);
